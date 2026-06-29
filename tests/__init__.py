dc5244fc2a5e7dc9dca8d47d682758eb1bb1ea22
Lucas Dias <lucas.azevedodias@outlook.com>
2026-06-29 11:13:28 -0300

    feat/Added tests structure

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2026 Lucas Dias
+
+"""Test suite for the project.
+
+This package contains all automated unit tests used to validate the
+correctness, performance, and behavior of the codebase.
+"""
